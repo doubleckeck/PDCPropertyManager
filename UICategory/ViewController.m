b7c2a8320d0b139bb2cff3acf976898efacaf6aa
PDC <doublecheck@yeah.net>
2016-06-04 16:45:54 +0800

    change readme file

--- UICategory/ViewController.m
+++ UICategory/ViewController.m
@@ -34,7 +34,6 @@
     self.myView.pdc_manager.backgroundColor([UIColor redColor]);
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         self.myView.pdc_manager
-        
         .center(CGPointMake(200, 200))
         .backgroundColor([UIColor orangeColor]);
         
